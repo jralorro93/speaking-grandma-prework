ef4cb3e3c8c25366d9ecc5d9cbacbc9c74ae67d1
Juan Alorro <jralorro93@gmail.com>
2019-01-17 18:43:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,6 +20,8 @@ def speak_to_grandma(phrase)
     return "HUH? SPEAK UP, SONNY!"
   elsif phrase == phrase.downcase
     return "HUH? SPEAK UP, SONNY!"
-  
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
   end
 end
