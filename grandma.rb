0fef3ac58619c8db1da5bb888008df41872e316b
Juan Alorro <jralorro93@gmail.com>
2019-01-17 18:47:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,20 +14,6 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else
-    return "NO, NOT SINCE 1938!"
-  end
-end
-
-def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.downcase
@@ -36,7 +22,6 @@ def speak_to_grandma(phrase)
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
     else
   		return "NO, NOT SINCE 1938!"
     end
